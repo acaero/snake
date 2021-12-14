5fbbc339d8ecc9205f99f42e3d2b03b8082299d8
acaero <davidaronschaefer@gmail.com>
2021-12-14 08:36:57 +0100

    Alle Punkte bis 8. implementiert

--- snake.py
+++ snake.py
@@ -5,11 +5,89 @@ from typing import Optional, List, Tuple
 TILE_SIZE = 20
 
 # TODO: Spielklassen
+class Item:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x * TILE_SIZE
+        self.y = y * TILE_SIZE
+
+    def occupies(self, x: int, y: int):
+        return x == self.x and y == self.y
+
+
+class Brick(Item):
+    def __init__(self, x: int, y: int) -> None:
+        super().__init__(x, y)
+
+    def draw(self, surface: pygame.Surface) -> None:
+        surface.fill(
+            (60, 63, 64),
+            pygame.Rect(
+                self.x,
+                self.y,
+                TILE_SIZE,
+                TILE_SIZE
+            )
+        )
+
+
+class Snake:
+    def __init__(self, x: int, y: int) -> None:
+        self._ocupies = [(x * TILE_SIZE, y * TILE_SIZE)]
+        self._direction = (1, 0)
+        self._grow = 0
+        self._last_direction = (1, 0)
+
+        self.grow(3)
+
+    def get_head(self) -> Tuple[int, int]:
+        return self._ocupies[0]
+
+    def set_direction(self, x: int, y: int) -> None:
+        if self._last_direction[0] + x != 0 and self._last_direction[1] + y != 0:
+            self._last_direction = self._direction
+            self._direction = (x, y)
+
+    def grow(self, n: int) -> None:
+        self._grow += n
+
+    def step(self, forbidden: List) -> bool:
+        next_tile = self._ocupies[0][0] + self._direction[0]* TILE_SIZE, self._ocupies[0][1] + self._direction[1] * TILE_SIZE
+
+        if self.occupied(next_tile[0], next_tile[1]):
+            return False
+
+        for brick in forbidden:
+            if brick.occupies(next_tile[0], next_tile[1]):
+                return False
+
+        self._ocupies.insert(0, next_tile)
+
+        if self._grow != 0:
+            self._grow -= 1
+        else:
+            self._ocupies.pop()
+
+        return True
+
+    def occupied(self, x: int, y: int) -> bool:
+        return (x, y) in self._ocupies
+
+    def draw(self, surface: pygame.Surface) -> None:
+        for tile in self._ocupies:
+            surface.fill(
+                (216, 217, 204),
+                pygame.Rect(
+                    tile[0],
+                    tile[1],
+                    TILE_SIZE,
+                    TILE_SIZE
+                )
+            )
 
 
 def main():
-    width = 20
-    height = 15
+    width = 50
+    height = 25
     speed = 7
 
     pygame.init()
@@ -21,12 +99,26 @@ def main():
     clock = pygame.time.Clock()
 
     # TODO: Spielobjekte anlegen
+    wall = [Brick(x, y)
+            for x in range(width)
+            for y in range(height)
+            if x == 0 or x == width - 1 or y == 0 or y == height - 1 or (x == random.randint(1, width - 1) and y != int(height / 2))
+            ]
+
+    snake = Snake(int(width / 2), int(height / 2))
 
     running = True
     while running:
-        screen.fill((20, 20, 20))
+        screen.fill((114, 115, 110))
 
         # TODO: Mauer zeichnen
+        for brick in wall:
+            brick.draw(screen)
+
+        snake.draw(screen)
+
+        if not snake.step(wall):
+            running = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -36,16 +128,16 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     # TODO: Richtung aendern: nach links
-                    pass
+                    snake.set_direction(-1, 0)
                 elif event.key == pygame.K_RIGHT:
                     # TODO: Richtung aendern: nach rechts
-                    pass
+                    snake.set_direction(1, 0)
                 elif event.key == pygame.K_UP:
                     # TODO: Richtung aendern: nach oben
-                    pass
+                    snake.set_direction(0, -1)
                 elif event.key == pygame.K_DOWN:
                     # TODO: Richtung aendern: nach unten
-                    pass
+                    snake.set_direction(0, 1)
 
         if not running:
             break
